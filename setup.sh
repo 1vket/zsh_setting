11d0d664125ded163a9a6b2d93839b2e8aefecd3
1vket <ivket3@gmail.com>
2022-02-11 17:38:09 +0900

    Add: rm command

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ echo "if [ -f ~/zsh_setting/.myzshrc ]; then" >> ~/.zshrc
 echo "   . ~/zsh_setting/.myzshrc" >> ~/.zshrc
 echo "fi" >> ~/.zshrc
 
+# remove
+rm -f ~/.vimrc
+rm -f ~/.tmux.conf
+rm -f ~/.vim/dein.toml
+rm -f ~/.vim/dein_lazy.toml
+
 # link
 mkdir -p ~/.vim
 ln -s ~/zsh_setting/.vimrc ~/.vimrc
